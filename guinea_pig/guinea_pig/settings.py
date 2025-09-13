5b057ba72f3980dd3c4581dc25ed4777466f83fc
Allan <allanhender@gmail.com>
2025-09-13 21:34:56 +0100

    linking to local postgres

--- guinea_pig/guinea_pig/settings.py
+++ guinea_pig/guinea_pig/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'guinea_pig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'guinea-pig-local',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
