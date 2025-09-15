20e41b62f5b42438e68332c21dc04859c4ce6cb7
Allan <allanhender@gmail.com>
2025-09-15 22:00:49 +0100

    working on daily investing funciton

--- guinea_pig/portfolio_tester/investing_funcitons.py
+++ guinea_pig/portfolio_tester/investing_funcitons.py
@@ -1,12 +1,28 @@
+import datetime
 
-
-def invest_daily(FTSE_weight,SNP500_weight,queryset):
+def invest_daily(amount_daily, start_date, end_date, FTSE_weight=0, SNP500_weight=0, FTSE_queryset=None, SNP500_queryset=None):
     total_shares = 0
     total_invested = 0
     iShares_World_total_shares = 0
     iShares_SNP500_total_shares = 0
     Vanguard_Global_Bond_total_shares = 0
     Cash_total = 0
+    value=0
 
-    for daily_entry in queryset:
-        
+    timeframe = end_date - start_date
+    total_days = timeframe.days 
+    total_amount_to_invest = total_days*amount_daily
+    
+    aggregated_amount_per_day = total_amount_to_invest/(len(FTSE_queryset)+1)
+    
+
+    print(aggregated_amount_per_day)
+
+    for daily_entry in FTSE_queryset:
+        pass
+        total_shares += (aggregated_amount_per_day/float(daily_entry.close))*FTSE_weight
+        value = total_shares*float(daily_entry.close)
+
+    return round(value,2)
+
+
--- guinea_pig/portfolio_tester/views.py
+++ guinea_pig/portfolio_tester/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import FtseData, Snp500Data
+from .investing_funcitons import invest_daily
 import datetime
 
 
@@ -8,13 +9,9 @@ import datetime
 
 def portfolio_tester_home(request):
     start_date = datetime.date(2024, 1, 1)
-    end_date = datetime.date(2024, 2, 2)
+    end_date = datetime.date(2024, 2, 1)
     queryset=FtseData.objects.filter(date__range=(start_date,end_date))
 
+    total_value = invest_daily(1,start_date,end_date,FTSE_weight=1,FTSE_queryset=queryset)
 
-
-
-
-
-    
-    return render(request, 'portfolio_tester/portfolio_tester_home.html', {'newdata': queryset})
+    return render(request, 'portfolio_tester/portfolio_tester_home.html', {'newdata': queryset, 'total_value': total_value})
