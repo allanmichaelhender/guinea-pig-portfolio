01eb8ba352c35f5c9d917d4eaa5105eac5ac097c
Allan <allanhender@gmail.com>
2025-09-23 18:44:10 +0100

    updating invester form

--- portfolio_tester/views.py
+++ portfolio_tester/views.py
@@ -22,9 +22,7 @@ def portfolio_tester_home(request):
 def portfolio_creator(request):
     if request.method == 'POST':
         form = PortfolioForm(request.POST)
-        print("trying")
         if form.is_valid():
-            print("fwaefaw")
             return redirect("portfolio_tester:tester",preserve_request=True)
     else:
         form = PortfolioForm()
@@ -34,15 +32,27 @@ def portfolio_creator(request):
 def tester(request):
     form = PortfolioForm(request.POST)
     if form.is_valid():
-        print("here")
-        PorfolioObject = form.save(commit=False)
+        PortfolioObject = form.save(commit=False)
         if request.user.is_authenticated:
-            PorfolioObject.user = request.user
+            PortfolioObject.user = request.user
         else: 
             User = get_user_model()
-            PorfolioObject.user = User.objects.get(pk=2)
-        PorfolioObject.save()
-        return render(request, 'portfolio_tester/portfolio_tester_home.html')
+            PortfolioObject.user = User.objects.get(pk=2)
+        
+        start_date = PortfolioObject.start_date
+        end_date = PortfolioObject.end_date
+        FTSE_queryset=FtseData.objects.filter(date__range=(start_date,end_date)).order_by('date')
+        SNP500_queryset=Snp500Data.objects.filter(date__range=(start_date,end_date)).order_by('date')
+        NIKKEI225_queryset=Nikkei225Data.objects.filter(date__range=(start_date,end_date)).order_by('date')
+
+        return_array = invest_daily(1,start_date,end_date,FTSE_weight=0.3,FTSE_queryset=FTSE_queryset,SNP500_weight=0.3,SNP500_queryset=SNP500_queryset,NIKKEI225_weight=0.4,NIKKEI225_queryset=NIKKEI225_queryset)
+
+        PortfolioObject.total_amount_invested = return_array[0]
+        PortfolioObject.final_amount = return_array[4]
+        PortfolioObject.change_percentage = return_array[0]*100/return_array[4]
+
+        PortfolioObject.save()
+        return redirect('portfolio_tester:my_portfolios')
     
 def my_portfolios(request):
     if request.user.is_authenticated:
