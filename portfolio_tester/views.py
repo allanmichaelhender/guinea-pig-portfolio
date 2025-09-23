7c861b787137ddbb99bd82ddfaa5fd5bfc6cb1f3
Allan <allanhender@gmail.com>
2025-09-23 20:54:14 +0100

    fixing montly investing

--- portfolio_tester/views.py
+++ portfolio_tester/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from .models import FtseData, Snp500Data, Nikkei225Data, Portfolios
-from .investing_funcitons import invest_daily, invest_monthly
+from .investing_funcitons import invest_daily, invest_monthly, test
 import datetime
 from .forms import PortfolioForm
 from django.db.models import F, Window
@@ -38,9 +38,22 @@ def tester(request):
             SNP500_queryset=Snp500Data.objects.filter(date__range=(start_date,end_date)).order_by('date')
             NIKKEI225_queryset=Nikkei225Data.objects.filter(date__range=(start_date,end_date)).order_by('date')
 
-            return_array = invest_daily(investment_amount,start_date,end_date,FTSE_weight=0.3,FTSE_queryset=FTSE_queryset,SNP500_weight=0.3,SNP500_queryset=SNP500_queryset,NIKKEI225_weight=0.4,NIKKEI225_queryset=NIKKEI225_queryset)
+            FTSE_weight = PortfolioObject.FTSE_weight
+            SNP500_weight = PortfolioObject.SNP500_weight
+            NIKKEI_weight = PortfolioObject.NIKKEI225_weight
 
 
+            return_array = invest_daily(investment_amount,
+                                        start_date,
+                                        end_date,
+                                        FTSE_weight,
+                                        SNP500_weight,
+                                        NIKKEI_weight,
+                                        FTSE_queryset,
+                                        SNP500_queryset,
+                                        NIKKEI225_queryset)
+
+            print("test")
             PortfolioObject.total_amount_invested = return_array[0]
             PortfolioObject.final_amount = return_array[4]
             change_percentage = (return_array[4]*100/float(return_array[0]))
@@ -58,12 +71,16 @@ def tester(request):
             partition_by=[ExtractYear('date'), ExtractMonth('date')],
             order_by=F('date').asc(),)).filter(row_number=1)
 
+            FTSE_monthly_queryset = FTSE_monthly_queryset.filter(date__range=(start_date,end_date)).order_by('date')
+
             SNP500_monthly_queryset = Snp500Data.objects.annotate(
     # Create partitions by year and month
             row_number=Window(
             expression=RowNumber(),
             partition_by=[ExtractYear('date'), ExtractMonth('date')],
             order_by=F('date').asc(),)).filter(row_number=1)
+
+            SNP500_monthly_queryset = SNP500_monthly_queryset.filter(date__range=(start_date,end_date)).order_by('date')
 
             NIKKEI225_monthly_queryset = Nikkei225Data.objects.annotate(
     # Create partitions by year and month
@@ -72,8 +89,22 @@ def tester(request):
             partition_by=[ExtractYear('date'), ExtractMonth('date')],
             order_by=F('date').asc(),)).filter(row_number=1)
 
-            return_array = invest_monthly(investment_amount,start_date,end_date,FTSE_weight=0.3,FTSE_queryset=FTSE_queryset,SNP500_weight=0.3,SNP500_queryset=SNP500_queryset,NIKKEI225_weight=0.4,NIKKEI225_queryset=NIKKEI225_queryset)
+            NIKKEI225_monthly_queryset = NIKKEI225_monthly_queryset.filter(date__range=(start_date,end_date)).order_by('date')
 
+            FTSE_weight = PortfolioObject.FTSE_weight
+            SNP500_weight = PortfolioObject.SNP500_weight
+            NIKKEI_weight = PortfolioObject.NIKKEI225_weight
+
+
+            return_array = invest_monthly(investment_amount,
+                                        start_date,
+                                        end_date,
+                                        FTSE_weight,
+                                        SNP500_weight,
+                                        NIKKEI_weight,
+                                        FTSE_monthly_queryset,
+                                        SNP500_monthly_queryset,
+                                        NIKKEI225_monthly_queryset)
 
             PortfolioObject.total_amount_invested = return_array[0]
             PortfolioObject.final_amount = return_array[4]
@@ -82,8 +113,6 @@ def tester(request):
             PortfolioObject.save()
 
             return redirect('portfolio_tester:my_portfolios')
-
-
 
 
 
