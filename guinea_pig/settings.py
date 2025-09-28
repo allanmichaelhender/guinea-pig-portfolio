e8fd0d24e314d53afc8e33b0aeb5d654a5b6208c
Allan <allanhender@gmail.com>
2025-09-28 16:28:52 +0100

    Update settings.py

--- guinea_pig/settings.py
+++ guinea_pig/settings.py
@@ -19,10 +19,6 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-pbiungywg!g1(4e3rvicb#a#iepgep6^zn8y0r%ji+f6fm+d#0'
 
@@ -137,10 +133,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
