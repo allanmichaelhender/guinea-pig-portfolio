3d1044c3d8c8fa2c402616b632b31cfb59850f22
Allan <allanhender@gmail.com>
2025-09-28 21:04:42 +0100

    cleaning up html/javascript/css

--- guinea_pig/urls.py
+++ guinea_pig/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name="homepage"),
+    path('', views.about, name="aboutpage"),
     path('users/', include('users.urls')),
     path('portfolio_tester/', include('portfolio_tester.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
--- portfolio_tester/forms.py
+++ portfolio_tester/forms.py
@@ -1,21 +1,17 @@
-from .models import Portfolios
 import datetime
 from django import forms
 from django.core.exceptions import ValidationError
-
-frequency_choices = (('daily','Daily'), ('monthly', 'Monthly'))
+from .models import Portfolios
 
 class PortfolioForm(forms.ModelForm):
     class Meta:
         model = Portfolios
         exclude = ["final_amount", "change_percentage", "total_amount_invested", "user"]
-        #fields = "__all__"
-        
         
         widgets = {
-            'investment_frequency': forms.RadioSelect(),
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'investment_frequency': forms.RadioSelect(attrs={'class': 'investment-frequency-input-field'}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'date-input-field'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'date-input-field'}),
             'FTSE_weight': forms.NumberInput(attrs={'step': '0.01', 'class': 'weight-input'}),
             'SNP500_weight': forms.NumberInput(attrs={'step': '0.01', 'class': 'weight-input'}),
             'NIKKEI225_weight': forms.NumberInput(attrs={'step': '0.01', 'class': 'weight-input'}),
@@ -28,21 +24,33 @@ class PortfolioForm(forms.ModelForm):
             'NIKKEI225_weight': 0,
         }
 
-        def clean(self):
-            cleaned_data = super().clean()
-            start_date = cleaned_data.get("start_date")
-            end_date = cleaned_data.get("end_date")
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
 
+        if start_date and end_date:
+            if start_date > end_date:
+                raise ValidationError("Start date cannot be after end date.")
+            
             min_date = datetime.date(2015, 1, 1)
             max_date = datetime.date(2025, 9, 1)
+            if not (min_date <= start_date <= max_date and min_date <= end_date <= max_date):
+                raise ValidationError(
+                    f"Dates must be between {min_date.strftime('%Y-%m-%d')} and {max_date.strftime('%Y-%m-%d')}."
+                )
 
-            if start_date and end_date:
-                if start_date > end_date:
-                    raise ValidationError("Start date cannot be after end date.")
-                
-                if not (min_date <= start_date <= max_date and min_date <= end_date <= max_date):
-                    raise ValidationError(
-                        f"Dates must be between {min_date.strftime('%Y-%m-%d')} and {max_date.strftime('%Y-%m-%d')}."
-                    )
+        ftse_weight = cleaned_data.get("FTSE_weight")
+        snp_weight = cleaned_data.get("SNP500_weight")
+        nikkei_weight = cleaned_data.get("NIKKEI225_weight")
 
-            return cleaned_data
+        weights = [ftse_weight, snp_weight, nikkei_weight]
+        
+        if all(w is not None for w in weights):
+            if any(w < 0 for w in weights):
+                raise ValidationError("Weights cannot be negative.")
+
+            if not (sum(weights) == 1):
+                raise ValidationError("The sum of the three weights must equal 1.")
+
+        return cleaned_data
